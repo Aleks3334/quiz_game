54bb4c1fd497dbf44ae2c609d2d83556ea825256
Aleks3334 <aleksandrdimitrov176@gmail.com>
2025-06-20 20:38:53 +0300

    added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
-let quizCategory = "Technology"
+//Initial data
 
-const getRandomQuestion = () => {
-    const categoryQuestions = questions.find(object => object.category.toLowerCase() === quizCategory.toLowerCase()).questions
-    const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)]
+let selectCategory = ""
+let selectQuestionCount = 0
+let currentQuestion = 0
+let score = 0
 
-    console.log(randomQuestion);
+//DOM Elements
 
-}
+const configContainer = document.querySelector(".config-container")
+const categoryOptions = document.querySelectorAll(".category-option")
+const questionOptions = document.querySelectorAll(".question-option")
+const startQuizBtn = document.querySelector(".start-quiz-btn")
 
+const quizContainer = document.querySelector(".quiz-container")
+const timeDuration = document.querySelector(".time-duration")
+const quizQuestion = document.querySelector(".quiz-question")
+const answerOption = document.querySelectorAll(".answer-option")
+const questionStatus = document.querySelector(".question-status")
+const nextQuestionBtn = document.querySelector("next-question-btn")
 
-const renderQuestion = () =>{
-    
-}
+const resultContainer = document.querySelector(".result-container")
+const resultMessage = document.querySelector(".result-message")
+const trAgainBtn = document.querySelector(".try-again-btn")
 
+//Select Category
 
-getRandomQuestion()
+categoryOptions.forEach(btn => {
+    btn.addEventListener("click", () => {
+        categoryOptions.forEach(button => button.classList.remove("active"))
+        btn.classList.add("active")
+        selectCategory = btn.textContent.trim()
+    })
+})
+
+questionOptions.forEach(btn => {
+    btn.addEventListener("click", () => {
+        questionOptions.forEach(button => button.classList.remove("active"))btn.classList.add("active")
+        selectQuestionCount = btn.textContent
+    })
+})
+
